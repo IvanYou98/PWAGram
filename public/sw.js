eb389de8f009daf34b06fcebdf4b4a4f4a6baf9d
IvanYou98 <yuanfanyou@gmail.com>
2022-06-04 23:28:56 -0400

    Add Dynamic Caching

--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,22 @@
+const LATEST_STATIC_CACHE = 'static-v6'
+const LATEST_DYNAMIC_CACHE = 'dynamic'
+
 self.addEventListener('install', function (event) {
     console.log('[Service Worker] Installing Service Worker ...', event);
     event.waitUntil(
-        caches.open('static')
+        caches.open(LATEST_STATIC_CACHE)
             .then(cache => {
                 console.log('[Servie Worker]: precaching app shell...');
                 cache.addAll([
                     '/',
                     '/src/js/app.js',
-                    'index.html'
+                    '/src/js/feed.js',
+                    '/src/js/fetch.js',
+                    '/src/js/promise.js',
+                    '/index.html',
+                    '/src/css/app.css',
+                    '/src/css/feed.css',
+                    '/src/images/main-image.jpg'
                 ])
             })
     )
@@ -16,16 +25,33 @@ self.addEventListener('install', function (event) {
 
 self.addEventListener('activate', function (event) {
     console.log('[Service Worker] Activating Service Worker ...', event);
+    event.waitUntil(
+        caches.keys().then(keyList => {
+                Promise.all(keyList.map(key => {
+                    if (key !== LATEST_STATIC_CACHE && key !== LATEST_DYNAMIC_CACHE) {
+                        return caches.delete(key);
+                    }
+                }))
+            }
+        )
+    )
     return self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
     const request = event.request;
     event.respondWith(
-        caches.match(request)
-            .then(response => {
-                // check if we have a valid response
-                return response ? response : fetch(request);
+        caches.match(request).then(matchResult => {
+            // check if we have a valid matchResult
+            if (matchResult) return matchResult
+            fetch(request).then(res => {
+                caches.open(LATEST_DYNAMIC_CACHE).then(cache => {
+                    cache.put(request.url, res.clone());
+                    return res;
+                })
+            }).catch(err => {
+                console.log(err);
             })
+        })
     );
 })
